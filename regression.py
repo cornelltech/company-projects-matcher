3ec16ddecb8abd7d42db63fece453d35ad27fb3f
ameyaacharya <apa52@cornell.edu>
2014-06-16 13:29:39 -0400

    Regression for all combinations of numerical variables.

--- regression.py
+++ regression.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np 
 import statsmodels.api as sm 
 
-def do_regression(file):
+def do_all_regressions(file):
 	data = pd.read_csv(file)
 	#data_array = np.array(data)
 	
@@ -13,11 +13,41 @@ def do_regression(file):
 	# group_experience = data_array[:,4]
 	# m_group_experience = data_array[:,5]
 
-	x = data[['num_yrs_work_experience', 'group_experience', 'm_group_experience']]
-	y = data['coding_ability']
+	# do_three_regression('coding_ability','num_yrs_work_experience', 'group_experience', 'm_group_experience', data)
 
+	we = 'num_yrs_work_experience'
+	ge = 'group_experience'
+	mge = 'm_group_experience'
+	ca = 'coding_ability'
+
+
+	print "Predict coding ability through work experience, group experience, and m_group experience"
+	x = data[[we, ge, mge]]
+	y = data[ca]
+	do_regression(x, y)
+	print ""
+
+	print "Predict group experience through work experience, coding ability, and m_group experience"
+	x1 = data[[we, ca, mge]]
+	y1 = data[ge]
+	do_regression(x1, y1)
+	print ""
+
+	print "Predict m_group experience through work experience, coding ability, and group experience"
+	x2 = data[[we, ca, ge]]
+	y2 = data[mge]
+	do_regression(x2, y2)
+	print ""
+
+	print "Predict work experience through group experience, coding ability, and m_group experience"
+	x3 = data[[ca, mge, ge]]
+	y3 = data[we]
+	do_regression(x3, y3)
+	print ""
+
+def do_regression(x, y):
 	estimate = sm.OLS(y, x).fit()
 	print estimate.summary()
 
 if __name__ == "__main__":
-	do_regression('survey_responses.csv')
+	do_all_regressions('survey_responses.csv')
