4f496c2f55be489a6d6b78bc04b3c9e14bcc21d1
ameyaacharya <apa52@cornell.edu>
2014-06-28 21:17:46 -0400

    Input is from survey_responses.csv, but still getting weird output.

--- covariance.py
+++ covariance.py
@@ -1,7 +1,9 @@
 import teams
 import numpy as np
+import pandas as pd
 from sklearn import preprocessing
 from scipy import linalg
+import clustering
 
 def preprocess_random_data(data):
 	enc = preprocessing.OneHotEncoder(categorical_features = [True, False, True, True, False])
@@ -23,24 +25,35 @@ def preprocess_random_data(data):
 	return one_hot_data
 
 if (__name__ == "__main__"):
+
+	# The data from the file that we want
+	data_array = clustering.__init__("survey_responses.csv")
+	one_hot_data_one = clustering.do_preprocessing(data_array)
+	
+	# Tried to calculate covariance based on the nroamlized
+	normalized = clustering.do_normalize(one_hot_data_one)
+	print "normalized is "
+	print normalized
+
 	# The vectors come in as
 	# (degree_pursuing, cod_abil, cs_ug, type_tech_stren, num_yrs_work_exp)
-	a = teams.create_random_student_vectors(50)
-	n = np.array(a)
+	# a = teams.create_random_student_vectors(50)
+	# n = np.array(a)
 
-	# After preprocessing, the vector is
-	# (degree_pursuing, cs_ug, type_tech_stren, cod_abil, num_yrs_work_exp)
-	one_hot_data = preprocess_random_data(n)
+	# # After preprocessing, the vector is
+	# # (degree_pursuing, cs_ug, type_tech_stren, cod_abil, num_yrs_work_exp)
+	# one_hot_data = preprocess_random_data(n)
 	 
-	covariance = np.cov(one_hot_data)
+	#covariance = np.cov(one_hot_data_one)
+	covariance = np.cov(normalized)
 	print covariance
 
-	# Calcuate matrix square root
+	#Calcuate matrix square root (REALLY small and has j's in it, even for normalized data)
 	matrix_square_root = linalg.sqrtm(covariance)
 	print matrix_square_root
 
-	# Calculate the pseudoinverse of a matrix.
-	pseudo_inverse = linalg.pinv(matrix_square_root)
-	print pseudo_inverse
+	# # Calculate the pseudoinverse of a matrix.
+	# pseudo_inverse = linalg.pinv(matrix_square_root)
+	# print pseudo_inverse
 
 # Need to do one hot encoding  on these categories first 
