0d1be860f8e5d26f66fc6e4f831585ee874c3bd6
ameyaacharya <apa52@cornell.edu>
2014-06-30 15:26:41 -0400

    Added check for eigenvalues, which complex (w. negative real parts). Seeing where the problem is. Unsure of how to fix.

--- covariance.py
+++ covariance.py
@@ -4,8 +4,6 @@ import pandas as pd
 from sklearn import preprocessing
 from scipy import linalg
 from scipy import spatial
-import statsmodels
-from statsmodels import stats
 from statsmodels.stats import correlation_tools
 import clustering
 
@@ -15,7 +13,6 @@ def preprocess_random_data(data):
 
 	#For 'survey_responses.csv,' this produces a 49 x 15 matrix. The last 4 columns are our quantitative data.
 	one_hot_data = enc.transform(data).toarray()
-
 
 	print "The parameters are: " + str(enc.get_params())
 	
@@ -27,10 +24,8 @@ def preprocess_random_data(data):
 	
 	print "The one hot data is " 
 	print one_hot_data
-	return one_hot_data
 
-	print "One hot data[0] is "
-	print one_hot_data[0]
+	return one_hot_data
 
 def is_positive_semidefinite(matrix):
 	def check_row(row):
@@ -40,41 +35,25 @@ def is_positive_semidefinite(matrix):
 	print res
 	return len(res) == 0
 
+def print_det_and_error(cov_one, cov_two):
+	print "Determinant of the nearest correlation matrix is: "
+	print linalg.det(cov_two)
+	print "Error is: "
+	print (cov_one - cov_two)
+
 def do_mahal_distance(file, use_pseudo_inv = True):
 	data_array = clustering.__init__(file)
 	
-	# After preprocessing, the vector is
 	# (degree_pursuing, cs_ug, type_tech_stren, cod_abil, num_yrs_work_exp)
 	one_hot_data_preprocessed = clustering.do_preprocessing(data_array)
 
 	covariance_matrix = np.cov(one_hot_data_preprocessed)
-	#print "Covariance matrix is: "
-	#print covariance_matrix
 
-
-	print "On original matrix: "
 	if (not (is_positive_semidefinite(covariance_matrix))):
-		#error = ("Covariance matrix of data is not positive semidefinite, i.e., there exist " +
-		#		 "negative eigenvalues. Fix this by applying a linear translation to input.")
-		#raise teams.FunctionError(error)
-
-		# Tried to use this, but it didn't produce a positive semidefinite matrix either.
-		# print "The NEAREST covariance matrix is "
-		# covariance_matrix = correlation_tools.cov_nearest(covariance_matrix)
-
-		# 
-
+		# Could use cov_nearest, but that doesn't produce a positive semidefinite matrix.
 		covariance_matrix_two = correlation_tools.corr_nearest(covariance_matrix)
-		# print covariance_matrix_two - covariance_matrix
-
-		print "Determinant of the nearest correlation matrix is "
-		print linalg.det(covariance_matrix_two)
-		# print covariance_matrix_two
-		# print "THE BOOLEAN IS "
-		#print "THE ERROR is "
-		#e =  covariance_matrix - covariance_matrix_two
-		#print e
-		pass
+		print_det_and_error(covariance_matrix, covariance_matrix_two)
+		covariance_matrix = covariance_matrix_two
 
 	# Calculate the inverse of the covariance matrix.
 	if (not(use_pseudo_inv)):
@@ -84,16 +63,16 @@ def do_mahal_distance(file, use_pseudo_inv = True):
 
 	# Calculate the pseudoinverse of a matrix.
 	else:
-		# TODO: Eventually want to use the matrix square root at some pt
-		#  cov_inverse = linalg.pinv(matrix_square_root)
+		# TODO: Use the matrix square root.
+		# cov_inverse = linalg.pinv(matrix_square_root)
 		cov_inverse = linalg.pinv(covariance_matrix)
 		print "(Pseudo) inverse of the covariance matrix is: "
 		print cov_inverse
 
-	obs_zero = one_hot_data_preprocessed[0]
-	obs_one = one_hot_data_preprocessed[1]
-
-	print "Mahal distance is " + str(spatial.distance.mahalanobis(obs_zero, obs_one, cov_inverse))
+	# TODO: not sure if we even want to use this. Could go through steps like Serge listed.
+	# obs_zero = one_hot_data_preprocessed[0]
+	# obs_one = one_hot_data_preprocessed[1]
+	# print "Mahal distance is " + str(spatial.distance.mahalanobis(obs_zero, obs_one, cov_inverse))
 
 	return (one_hot_data_preprocessed, covariance_matrix_two)
 
@@ -101,19 +80,19 @@ if (__name__ == "__main__"):
 	np.set_printoptions(threshold=np.nan)
 
 	result = do_mahal_distance("survey_responses.csv")
-	# data = result[0]
-	# cov  = result[1]
+	data = result[0]
+	fixed_cov  = result[1]
 	
-	# print "On fixed: "
-	# print is_positive_semidefinite(cov)
-	# print cov
+	print "On fixed: "
+	print is_positive_semidefinite(fixed_cov)
 
-	# check_positive_definite(np.array([[-1, -2], [5, 6]]))
+	print "Eigenvalues are: "
+	print linalg.eig(fixed_cov)
 	
-
-	# Calcuate matrix square root (REALLY small and has j's in it, even for normalized data)
-	# matrix_square_root = linalg.sqrtm(covariance)
+	# Calcuate matrix square root (REALLY small and complex, even for normalized data)
+ 	# matrix_square_root = linalg.sqrtm(fixed_cov)
 	# print matrix_square_root
+
 
 	
 
