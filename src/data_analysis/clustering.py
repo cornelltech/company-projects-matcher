Fixed covariance. Removed print statement from clustering.

src/data_analysis/clustering.py
@@ -16,7 +16,7 @@ def __init__(file = default_file):
 	""" Extract data from CSV file and place into numpy array. """
 	initial_array = pd.read_csv(file)
 	data_array = np.array(initial_array)
-	print data_array
+	#print data_array
 	return data_array
 
 def do_preprocessing(data_array):

src/data_analysis/covariance.py
@@ -36,7 +36,7 @@ def preprocess_random_data(data):
 	return one_hot_data
 
 def is_positive_semidefinite(cov_matrix):
-	(eigenvalues, eigenvectors) = linalg.eig(cov)
+	(eigenvalues, eigenvectors) = linalg.eig(cov_matrix)
 	res = []
 	#print type(eigenvalues)
 	#print eigenvalues.shape
@@ -44,68 +44,68 @@ def is_positive_semidefinite(cov_matrix):
 		#print e
  		if (e < 0):
 	 		res.append(e)
-	print "Negative eigenvalues of covariance matrix are:"
-	print res
-	#return len(res) == 0
+	#print "Negative eigenvalues of covariance matrix are:"
+	#print res
+	return len(res) == 0
 
-def print_det_and_error(cov_one, cov_two):
-	print "Determinant of the nearest correlation matrix is: "
-	print linalg.det(cov_two)
-	print "Error is: "
-	print (cov_one - cov_two)
-
-def do_mahal_distance(student_one, student_two, use_pseudo_inv = True, file = default_file):
+# Returns (data, covariance matrix.)
+def create_covariance_matrix(file = default_file):
 	data_array = clustering.__init__(file)
-	
-	# (degree_pursuing, cs_ug, type_tech_stren, cod_abil, num_yrs_work_exp)
 	one_hot_data_preprocessed = clustering.do_preprocessing(data_array)
-	print type(one_hot_data_preprocessed)
-	print "One hot data preprocessed shape:"
-	print one_hot_data_preprocessed.shape
-
 	covariance_matrix = np.cov(one_hot_data_preprocessed)
-	print "Covariance matrix shape:"
 	shape = covariance_matrix.shape
-	print shape
 	num_rows = shape[0]
 	num_cols = shape[1]
-
+	
 	# Should never happen
 	if (not(num_rows == num_cols)):
 		raise CovarianceError("Covariance matrix is not a square matrix.")
 
 	else:
-		n = num_rows
-		i = np.matrix(np.identity(n))
-		print i
-		factor = 10. ** -10
-		mapped = np.multiply(i, factor)
-		#print mapped
-		#print type(mapped)
-		#print mapped.shape
-		result = np.add(mapped, covariance_matrix)
-		#print result
+		if (is_positive_semidefinite(covariance_matrix)):
+			pass		
+		# Our covariance matrix is not positive semidefinite -- an arithmetic error.
+		# Will add (a small number * the identity matrix) to covariance matrix to fix this error.
+		else:
+			n = num_rows
+			i = np.array(np.identity(n))
+			factor = 10. ** -10
+			# Create a matrix that is a small number times the identity.
+			small_identity = np.multiply(i, factor)
 
-	# if (not (is_positive_semidefinite(covariance_matrix))):
-	# 	# Could use cov_nearest, but that doesn't produce a positive semidefinite matrix.
-	# 	covariance_matrix_two = correlation_tools.corr_nearest(covariance_matrix)
-	# 	print_det_and_error(covariance_matrix, covariance_matrix_two)
-	# 	covariance_matrix = covariance_matrix_two
+			# Add that matrix to our covariance matrix (to make sure that our matrix is positive semidefinite.)
+			result = np.add(small_identity, covariance_matrix)
+			if (not(is_positive_semidefinite(result))):
+				raise CovarianceError("Fixed covariance matrix is not positive semidefinite.")
+			else:
+				covariance_matrix = result
 
+	return (one_hot_data_preprocessed, covariance_matrix)
+
+# Calcuate matrix square root 
+def sqrt_covariance_matrix(covariance_matrix):	
+ 	matrix_square_root = linalg.sqrtm(covariance_matrix)
+	return matrix_square_root
+
+def inverse_matrix(sqrt_covariance_matrix, use_pseudo_inv = True):
+	
+	# Calculate real matrix inverse.
+	if (not(use_pseudo_inv)):
+	 	cov_inverse = linalg.inv(sqrt_covariance_matrix)
+	 	print "(Real) inverse of the sqrt. covariance matrix is: "
+
+	# Calculate the matrix pseudoinverse.
+	else:
+	 	cov_inverse = np.linalg.pinv(sqrt_covariance_matrix)
+		print "(Pseudo) inverse of the sqrt. covariance matrix is: "
+
+	print cov_inverse
+	return cov_inverse
+	
+def do_mahal_distance(student_one, student_two, use_pseudo_inv = True, file = default_file):
+	
 	# Calculate the inverse of the covariance matrix.
-	# if (not(use_pseudo_inv)):
-	# 	cov_inverse = linalg.inv(covariance_matrix)
-	# 	print "(Real) inverse of the covariance matrix is: "
-	# 	print cov_inverse
-
-	# Calculate the pseudoinverse of a matrix.
-	# else:
-	# 	# TODO: Use the matrix square root.
-	# 	# cov_inverse = linalg.pinv(matrix_square_root)
-	# 	cov_inverse = linalg.pinv(covariance_matrix)
-		#print "(Pseudo) inverse of the covariance matrix is: "
-		#print cov_inverse
-
+	# 
 	# TODO: not sure if we even want to use this. Could go through steps like Serge listed.
 	# obs_zero = one_hot_data_preprocessed[0]
 	# obs_one = one_hot_data_preprocessed[1]
@@ -114,8 +114,7 @@ def do_mahal_distance(student_one, student_two, use_pseudo_inv = True, file = de
 	# TODO: should return the Mahalanobis distance between the data at the two indices.
 	# TODO TODO: should pass in a team, and return the sorted list of mahal distances at all points.
 
-	# TODO: this was covariance_matrix_two
-	return (one_hot_data_preprocessed, result)
+	pass
 
 # Pass in team of Students.
 # TODO: make a Team ID, and return (Team_ID, result.)
@@ -136,17 +135,20 @@ def do_and_sort_all_mahal_dists(set_of_teams):
 if (__name__ == "__main__"):
 	np.set_printoptions(threshold=np.nan)
 
-	result = do_mahal_distance(0, 1)
-	data = result[0]
-	cov  = result[1]
-	
-#	print "On fixed: "
-	is_positive_semidefinite(cov)
+	tup = create_covariance_matrix()
+	data = tup[0]
+	covariance_matrix  = tup[1]
+
+	sq_cov = sqrt_covariance_matrix(covariance_matrix)
+	c = np.multiply(sq_cov, sq_cov)
+	print "Covariance matrix is: "
+	print covariance_matrix
+	print "Sqrt multiplied by itself:"
+	print c
+	#print (c == covariance_matrix)
+	#inverse_matrix(sq_cov)
 
 #	print "Eigenvalues are: "
 #	print linalg.eig(cov)
-	
-	# Calcuate matrix square root (REALLY small and complex, even for normalized data)
- 	# matrix_square_root = linalg.sqrtm(fixed_cov)
-	# print matrix_square_root
+
 