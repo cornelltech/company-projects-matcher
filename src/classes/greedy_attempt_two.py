9f3d69fdedeb0d13e0886f4713acc3ea656496b9
ameyaacharya <apa52@cornell.edu>
2014-07-29 16:36:13 -0400

    Closes #27.

--- src/classes/greedy_attempt_two.py
+++ src/classes/greedy_attempt_two.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import pandas as pd 
+import random
 
 import classes
 import random_teams
@@ -62,15 +63,90 @@ def create_feasible_projects(students):
 def match_with_first_choice(students, projects):
 	return greedy_student_and_fix.match_with_first_choice(students, projects)
 
+def initial_solution(students, projects):
+	# The index of the ranking that we are currently looking at.
+	ranking_spot = 0
 
+	# The indices of the projects whose students were already removed from unmatched_students.
+	removed_projects = []
+
+	# TODO: might not want to actually modify the given list.
+	unmatched_students = students
+
+	while (len(unmatched_students) > 0 and ranking_spot < classes.number_project_rankings):
+		#random.shuffle(unmatched_students)
+		for student in unmatched_students:
+			cur_project_ID = student.project_rankings[ranking_spot]
+			cur_project = all_pairs_sorted.get_project_from_ID(cur_project_ID, projects)
+			print "Student " + str(student.ID)
+			print "Rank " + str(ranking_spot) + " is project " + str(cur_project_ID)
+			
+			# Try to add student to the project.
+			successful_add = cur_project.add_student(student)
+			if (successful_add):
+				print "Successful add of student " + str(student.ID) + " to project " + str(cur_project.ID)
+
+			# If there were no spots available, add this student to the waiting list.
+			else:
+				print "Not successful. Adding to waiting list"
+				cur_project.add_waiting_student(student)
+
+			# If the project is full and its students havent been 
+			# removed yet, then remove it and its students.
+			if (not (cur_project.has_remaining_spots())):
+				print "For project " + str(cur_project_ID) + ":"
+				print "Project " + str(cur_project_ID) + " has no more spots."
+				
+				if (not (cur_project.ID in removed_projects)):
+					print "The students on this project are: ",
+					print [s.ID for s in cur_project.students]
+					remove_students_from_projects(cur_project.students, projects, cur_project_ID)
+
+					print "Before remove: "
+					print "Unmatched students is:"
+					print [s.ID for s in unmatched_students]
+					print "Cur project students is: "
+					print [s.ID for s in cur_project.students]
+					# Remove students from unmatched_students
+					for student in cur_project.students:
+						unmatched_students.remove(student)
+
+					print "After remove: "
+					print "Unmatched students is:"
+					print [s.ID for s in unmatched_students]
+
+					removed_projects.append(cur_project.ID)
+				else:
+					pass
+
+		ranking_spot += 1
+
+	for project in projects:
+		if (not(project.students == [])):
+			print "For project " + str(project.ID) + ":"
+			print [s.ID for s in project.students]
+
+			# For all projects
+			# If this student ID is on the project
+				# Remove the student from that project
+			# If this student ID is on the waiting list
+				# Remove the student fromt that waiting list.
+
+def remove_students_from_projects(students_to_remove, projects, ID):
+	for project in projects:
+		if (not (project.ID == ID)):
+			for student in students_to_remove:
+				if student in project.students:
+					project.students.remove(student)
+				if student in project.waiting_students:
+					project.waiting_students.remove(student)
 
 # create all projects
 
 if __name__ == "__main__":
 	students = create_students_from_input("tests.csv")
 	all_projects = all_pairs_sorted.generate_all_projects()
+	initial_solution(students, all_projects)
 	#feasible_projects = create_feasible_projects(students)
-	first_choice_matches = match_with_first_choice(students, all_projects)
-	#print [names_projects[p.ID] for p in create_feasible_projects(students)]
 
 
