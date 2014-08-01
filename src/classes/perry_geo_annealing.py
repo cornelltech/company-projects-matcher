d1b038a1f01d3f29be4e8226f937b9eb5096dadd
ameyaacharya <apa52@cornell.edu>
2014-08-01 12:19:37 -0400

    Added initial attempt at the move function. Relies on full teams, noted in #38.

--- src/classes/perry_geo_annealing.py
+++ src/classes/perry_geo_annealing.py
@@ -1,7 +1,7 @@
 import numpy as np
+import random_teams
 
 # A function to calculate the energy of a state.
-# #38: did not include diversity yet. Will add that after.
 def energy(state):
 	projects = state[0]
 	unmatched_students = state[1]
@@ -27,6 +27,44 @@ def energy(state):
 		return 0
 
 	return avg_project_costs() + unmatched_student_cost() + team_diversity_cost()
+
+# A function to make a random change to a state.
+# Returns None (just like the example).
+# Currently implemented as one swap of two people across teams.
+def move(state):
+	projects = state[0]
+	#unmatched_students = state[1]
+
+	project_one = random_teams.random_project(projects)
+	project_two = random_teams.random_project(projects)
+
+	# Team to pick first students from 
+	pick_team = random_teams.random_two_choice()
+
+	if (pick_team == 0):
+		first_team = project_one
+		second_team = project_two
+	else:
+		first_team = project_two
+		second_team = project_one
+
+	# Pick a student from the first team, and this student will be swapped.
+	student_one = random_teams.random_student(first_team)
+	student_two = random_teams.random_student(second_team)
+
+	# NOTE: this is problematic if teams aren't full.
+	# 38
+	while (not (student_one.degree_pursuing == student_two.degree_pursuing)):
+		student_two = random_teams.random_student(second_team)
+
+	# Remove the students from their respective teams
+	first_team.students.remove(student_one)
+	second_team.students.remove(student_two)
+
+	first_team.add_student(student_two)
+	second_team.add_student(student_one)
+
+
 
 
 
