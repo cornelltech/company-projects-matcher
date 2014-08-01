4d49625e287fad3685d26a1f7c82aca5469372f9
ameyaacharya <apa52@cornell.edu>
2014-08-01 11:21:44 -0400

    Implementation of the functions necessary to run annealing.

---
+++ src/classes/perry_geo_annealing.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+# A function to calculate the energy of a state.
+# #38: did not include diversity yet. Will add that after.
+def energy(state):
+	projects = state[0]
+	unmatched_students = state[1]
+	# Averages the costs on each project, and then averages across all projects.
+	def avg_project_costs():
+		project_costs = []
+		for project in projects:
+			costs = []
+			for student in project.students:
+				rank = student.get_ranking(project.ID)
+				cost = student.get_cost_from_ranking(rank)
+				costs.append(cost)
+			avg_project_cost = np.mean(costs)
+			project_costs.append(avg_project_cost)
+		energy = np.mean(project_costs)
+		return energy
+
+	def unmatched_student_cost():
+		return 10000 * len(unmatched_students)
+
+	# 34
+	def team_diversity_cost():
+		return 0
+
+	return avg_project_costs() + unmatched_student_cost() + team_diversity_cost()
+
+
+
+
+
+
+
+
