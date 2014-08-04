cd75cf7620f39f0f6503d5bf8bcb85ccbd1ec981
ameyaacharya <apa52@cornell.edu>
2014-08-04 10:47:41 -0400

    Updated get_ranking to return the actual ranking, not the index of the project in the students' list (added a +1).

--- src/classes/classes.py
+++ src/classes/classes.py
@@ -208,7 +208,7 @@ class Student(object):
 		try:
 			rankings = (self._project_rankings).tolist()
 			ind = rankings.index(project_id)
-			return ind
+			return ind + 1
 		except(ValueError):
 			# Student did not rank this project.
 			return 1000000000000
