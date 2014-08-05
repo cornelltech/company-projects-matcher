f8709dcd8e573a5aa246d2d170b1bb8936b8e591
ameyaacharya <apa52@cornell.edu>
2014-08-05 16:35:28 -0400

    Removed functions and moved them to util.

--- matcher/src/classes/all_pairs_sorted.py
+++ matcher/src/classes/all_pairs_sorted.py
@@ -3,10 +3,9 @@ import pandas as pd
 
 import classes
 import itertools
+import util
 from classes import Student
-from classes import Project
 from classes import CompError
-from classes import FieldError
 
 student_ids = []
 
@@ -36,87 +35,6 @@ def normalize_bet_zero_and_one(lst):
 	final = [(elm * 1.0) / den for elm in num]
 	return final
 
-# For each of the IDs in classes' valid projects, create a project object with that ID.
-# As a default, each project requires 2 MBAs and 2 MEngs.
-def generate_all_projects(num_MBAs = 2, num_MEngs = 2):
-	projects_lst = []
-	for ID in classes.vals_valid_projects:
-		p = Project(ID, num_MBAs, num_MEngs)
-		projects_lst.append(p)
-	return projects_lst
-
-def get_project_from_ID(ID, projects):
-	matching_ID_lst = filter(lambda x: x.ID == ID, projects)
-	if (len(matching_ID_lst) == 0):
-		error = "ID " + str(ID) + " does not match to a valid project."
-		raise FieldError(error)
-	elif (len(matching_ID_lst) > 1):
-		error = "There is more than one matching project. Problem!"
-		raise FieldError(error)
-	
-	# Otherwise, there is one element in the list and it matches our desired ID.
-	else:
-		return matching_ID_lst[0]
-
-# Filter out projects with insufficient rankings to get matched.
-# Returns a list of projects which passed the test.
-def remove_infeasible_projects(students, projects, verbose = False):
-	insufficient_IDs = []
-	for p in projects:
-		matched = filter(lambda x: p.ID in x.project_rankings, students)
-		if (verbose):
-			print "For project " + str(p.ID) + ":"
-			print [s.ID for s in matched]
-		MBAs_ranked = [s for s in matched if s.degree_pursuing == "MBA"]
-		MEngs_ranked = [s for s in matched if s.degree_pursuing == "MEng"]
-		if (verbose):
-			print [s.ID for s in MBAs_ranked]
-			print [s.ID for s in MEngs_ranked]
-
-	 	if (len(MBAs_ranked) < p.num_MBAs or len(MEngs_ranked) < p.num_MEngs):
-	 		if (verbose):
-	 			string = "Not enough MBAs or MEngs ranked project "
-	 			string += str(p.ID)
-	 			string += " for it to be included."
-	 			print string
-	 		insufficient_IDs.append(p.ID)
-
-	projects = filter(lambda p: not(p.ID in insufficient_IDs), projects)
-	return projects
-
-# For a specific project p, get the students' overall interest in the project.
-# Higher interest means that more students ranked it highly on their lists.
-# The "interest" variable determines if how high a student ranked the project on their
-# list counts extra towards the ranking, or if it's just how many students ranked it.
-# Currently set the default so that its just how many students ranked it.
-def get_project_interest_from_rankings(p, students, verbose = False, interest = False):
-
-	# Check which students ranked this project.
-	matched = filter(lambda s: p.ID in s.project_rankings, students)
-	
-	if (verbose):
-		print "The following students ranked project " + str(p.ID) + ":"
-		print [s.ID for s in matched]
-
-	# For each student, get their interest in this project.
-	# Add this to the sum of the overall interest.
-	if (interest):
-		overall_interest = 0
-		for s in matched:
-			rank = s.get_ranking(p.ID)
-			interest = s.get_interest_from_ranking(rank)
-	 		overall_interest = overall_interest + interest
-		return overall_interest
-
-	else:
-		return len(matched)
-
-
-# Sort projects by highest demand to lowest demand.
-def sort_projects_by_demand(students, projects, interest = False):
-	projects.sort(key = lambda p: get_project_interest_from_rankings(p, students, interest = interest), reverse = True)
-	return projects
-
 
 # This function takes in the already-filtered list of projects.
 # Call remove infeasible projects on the original list of projects
@@ -138,7 +56,7 @@ def sort_and_match_by_pairs(projects, students, verbose = False):
 		print ""
 
 	# Sort projects in order of highest demand to lowest demand.
-	projects.sort(key = lambda p: get_project_interest_from_rankings(p, students), reverse = True)
+	projects.sort(key = lambda p: util.get_project_interest_from_rankings(p, students), reverse = True)
 
 	added_projects = []
 
@@ -311,7 +229,8 @@ def get_students_from_input(file, normalize=True):
 
 def do_tests():
 	students_lst = get_students_from_input("tests.csv")
-	projects_filtered = remove_infeasible_projects(students_lst)
+	all_projects = util.generate_all_projects()
+	projects_filtered = util.remove_infeasible_projects(students_lst, all_projects)
 	print [p.ID for p in projects_filtered]
 	#sort_and_match_by_pairs(projects_filtered, students_lst)
 
