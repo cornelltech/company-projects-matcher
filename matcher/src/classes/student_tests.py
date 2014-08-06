2ea5cc7ba92f8cbb3dfc76a727e074add6c28e41
ameyaacharya <apa52@cornell.edu>
2014-08-06 12:18:22 -0400

    Added more test cases.

--- matcher/src/classes/student_tests.py
+++ matcher/src/classes/student_tests.py
@@ -1,4 +1,5 @@
 from student import Student
+from student import Project
 
 # class MBAStudent(Student):
 
@@ -14,11 +15,14 @@ if __name__ == "__main__":
  # 	def __init__ (self, name, ID, degree_pursuing, cod_abil, cs_ug, num_yrs_work_exp):
 
  	# NOTE: This is going to error. Not imperative to fix this -- just a random test.
-	a = Student("Ameyayayaya", 2886650, 1, 0, 3, 6, [3055, 3250, 3250, 3250, 3250, 3250, 3250, 3250, 3250, 3250])
-	print a.name
-	print a.ID
-	print a.degree_pursuing
-	print a.coding_ability
-	print a.was_cs_ug
-	print a.work_experience
-	print a.project_rankings
+ 	lst = range(28, 39)
+ 	ranks_lst = map(lambda x: x * 65, lst)
+	a = Student("Ameyayayaya", 2886650, 1, 0, 3, 6, ranks_lst)
+	
+	# print a.name
+	# print a.ID
+	# print a.degree_pursuing
+	# print a.coding_ability
+	# print a.was_cs_ug
+	# print a.work_experience
+	# print a.project_rankings
