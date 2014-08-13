232b16fe543ecd7a6e77df94c45fd71def616045
ameyaacharya <apa52@cornell.edu>
2014-08-13 14:48:32 -0400

    Removed more unnecessary code.

--- matcher/src/classes/greedy_attempt_two.py
+++ matcher/src/classes/greedy_attempt_two.py
@@ -1,12 +1,8 @@
-import numpy as np 
-import pandas as pd 
 import util
 
 import classes
 import ConfigParser
-import greedy_student_and_fix
 import random
-from classes import Student
 from classes import CompError
 from classes import FieldError
 
@@ -14,16 +10,15 @@ student_ids = []
 
 def print_students(projects):
 	for project in projects:
-		#pass
-	#	if (len(project.students) >= allowable_team_size):
 		print "For project " + str(project.ID) + ":"
 		print "     Students: " + str([(s.ID, s.degree_pursuing) for s in project.students])
-		#print "     Waiting: " + str([(rank, s.ID) for (rank, s) in project.waiting_students])
 
-# Creates a random initial initial solution from only the feasible projects.
-# Have the option to sort the projects by the highest interest first, so there is a 
-# higher chance that the students matched to it actually ranked it.
 def make_initial_solution(students, unsorted_projects, num_MBAs, num_MEngs, sorted = False, verbose = False):
+	'''
+		Creates a random initial initial solution from only the feasible projects.
+		Have the option to sort the projects by the highest interest first, so there is a 
+		higher chance that the students matched to it actually ranked it.
+	'''
 
 	util.input_checks(students, unsorted_projects, num_MBAs, num_MEngs, sorted = False) 
 
@@ -302,236 +297,6 @@ def greedy_initial_solution_and_fill_unmatched(students, feasible_projects, verb
 	feasible_projects = randomly_add_unmatched_students(initial_res, verbose)
 	return [p for p in feasible_projects if len(p.students) > 0]
 
-def post_processing(feasible_projects, students, verbose = True):
-
-
-	# NEW APPROACH
-
-	# For each of the projects that are not full:
-		# Add the students that are waiting 
-			# (until all students are over or there is a full team)
-			# Sort the students by the highest rank first
-	for project in feasible_projects:
-		print str(project.ID) + ": " + str(project.has_spot_for_one_more())
-		
-		# These projects are not full.
-		if (project.has_remaining_spots()):
-		 	if (project.has_waiting_students()):
-		 		for (rank, waiting_student) in project.waiting_students:
-		 			# Will add as many students as it can, and will return false and do nothing
-		 			# when it cannot add any more waiting students.
-		 			# This is already sorted by highest rank first. 
-		 			project.add_student(waiting_student)
-		else:
-		 	pass
-	
-
-	# Sort the projects by number of spots remaining (lowest first).
-	new_lst = sorted(feasible_projects, key = lambda x: len(x.students), reverse = True)
-	feasible_projects = new_lst
-	print [(p.ID, len(p.students)) for p in feasible_projects]
-
-	# Allowable team size
-	team_size = feasible_projects[0].num_MBAs + feasible_projects[0].num_MBAs
-	allowable_team_size = team_size-1
-
-	# If the team size is 3 (or some allowable number):
-			# For all students
-				# Remove the student from other waiting lists
-				# Remove the student from other student lists
-
-	for project in feasible_projects:
-		if (len(project.students) >= allowable_team_size):
-			remove_students_from_projects(project.students, feasible_projects, project.ID)
-
-	# Creating a list of unfilled students
-	# And will assign these to different projects
-	unmatched_students = []
-
-
-	# For all of the remaining projects that are not size 3:
-	# Remove the students and add them to an unmatched list
-	for project in feasible_projects:
-		# These teams will not be full
-		if (len(project.students) < allowable_team_size):
-			# Add the project's students to the unmatched list
-			unmatched_student_IDs = [s.ID for s in unmatched_students]
-			for student in project.students:
-				if (not(student.ID in unmatched_student_IDs)):
-					unmatched_students.append(student)
-
-	print "Unmatched students is :"
-	print [s.ID for s in unmatched_students]
-
-	# Adds students to form teams of 5.
-	# for unmatched_student in unmatched_students:
-	# 	# If they are on the waiting list for some full teams:
-	# 		# Pick the highest rank that they gave a team whose waiting list they are on.
-	# 		# Add the student to that team.
-	# 	# If they are not on the waiting list:
-	# 		# Leave them unmatched.
-	# 	waiting_on = find_waiting_lists(unmatched_student, feasible_projects)
-	# 	if (len(waiting_on) > 0):
-	# 		top_project = waiting_on[0]
-	# 		top_project.students.append(unmatched_student)
-	# 		for w_tuple in top_project.waiting_students:
-	# 			if (w_tuple[1].ID == unmatched_student.ID):
-	# 				top_project.waiting_students.remove(w_tuple)
-	# 		unmatched_students.remove(unmatched_student)
-	# 		# Remove this student from other projects
-	# 		remove_students_from_projects([unmatched_student], projects, top_project.ID)
-
-	unfilled_projects_with_ranks = []
-	for project in feasible_projects:
-		# If the project is not full
-		if (len(project.students) < team_size):
-			# List all of the students who ranked it
-			project_ranks = []
-			for student in students:
-				if (project.ID in student.project_rankings):
-					rank = student.get_ranking(project.ID)
-					# These are the unmatched students who ranked the projects.
-					# Add the student, rank, and degree to project ranks.
-					project_ranks.append((student, rank, student.degree_pursuing))
-			# If any students ranked these projects:	
-			if (len(project_ranks) > 0):
-				# Add them to our list.
-				unfilled_projects_with_ranks.append((project, project_ranks))
-
-
-	# Print the unfilled projects that were ranked
-	for tup in unfilled_projects_with_ranks:
-		ID = tup[0]
-		project_ranks = tup[1]
-		print str(ID) + ":"
-		print [(rank, s.ID, deg) for (s, rank, deg) in project_ranks]
-		
-	print "Starting the sorted"
-	# Sort the unfilled projects by the number of students that they have (largest first)
-	# Then going to "steal" students who also ranked these from other projects
-	# And see if that resolves things
-	sorted_unfilled = sorted(unfilled_projects_with_ranks, 
-		key = lambda x: len(x[0].students), reverse = True)
-	for proj in sorted_unfilled:
-		students = proj[0].students
-		print str(proj[0].ID) + ":" + str(len(students))
-	print "Ending the sorted"
-
-	unmatched_IDs = [s.ID for s in unmatched_students]
-	#feasible_project_IDs = [s.ID for s in feasible_projects]
-
-	#for tup in sorted_unfilled:
-	while (len(unmatched_IDs) > 0):
-		# TODO: this is hacky and was a way to only affect the first project.
-		index = util.random_index(len(unmatched_IDs))
-		tup = sorted_unfilled[index]
-		project = tup[0]
-		project_ranks = tup[1]
-		# [(rank, s.ID, deg) for (s, rank, deg) in project_ranks]
-		# Find out what type of student the project requires
-		num_MBAs = filter(lambda s: s.degree_pursuing == "MBA" or s.degree_pursuing == 0, project.students)
-		print [s.ID for s in project.students]
-		num_MBAs_needed = 2 - len(num_MBAs)
-		num_MEngs = filter(lambda s: s.degree_pursuing == "MEng" or s.degree_pursuing == 1, project.students)
-		num_MEngs_needed = 2 - len(num_MEngs)
-
-		for i in range(0, num_MBAs_needed):
-			# Pick a random MBA from the list of students who ranked.
-			need_new_student = True
-			while (need_new_student):
-				r = util.random_index(len(project_ranks))
-				random_triple = project_ranks[r]
-				new_student = random_triple[0]
-				student_type = random_triple[2]
-				if (student_type == "MBA" or student_type == 0):
-					IDs_already_on_project = [s.ID for s in project.students]
-					# Check that this student is not already on this project
-					if (new_student.ID in IDs_already_on_project):
-						# If they are, then pick another random student.
-						pass
-					else:
-						# They're not on the project, so don't need to look for another student.
-						need_new_student = False
-						# Add the student to the project.
-						print "FOUND A SUITABLE MBA TO ADD "
-						print "SUITABLE MBA's ID IS " + str(new_student.ID)
-						boolean = project.add_student(new_student, verbose = True) 
-						# If this new student is on the waiting list.
-						waiting_list_IDs = [s.ID for (rank, s) in project.waiting_students]
-						if (new_student.ID in waiting_list_IDs):
-							# Remove the student from the project
-							project.waiting_students.remove(new_student)
-						print boolean
-
-						matched = not (new_student.ID in unmatched_IDs)
-						print "Student " + str(new_student.ID) + " is matched: " + str(matched)
-
-						# If this student was already matched, remove this student from their other project.
-						if (matched):
-							# Find the project that this student was on
-							project_to_remove = find_students_project(new_student, 
-								feasible_projects, project.ID)
-							# Remove this student from that project.
-							project_to_remove.students.remove(new_student)
-							# (HARD) Add that project to the list of projects that are fill.
-
-							# Change to a while loop. ^^^^^
-							pass
-
-						# If they weren't matched, remove them from the unmatched list.
-						else:
-							# Remove the student from the unmatched students list
-							unmatched_students.remove(new_student)
-							unmatched_IDs.remove(new_student.ID)
-				else:
-					pass
-
-		print "Project needed " + str(num_MBAs_needed) + " MBAs. And now is "
-		print str(project.ID) + ": " + str([s.ID for s in project.students])	 	
-
-		for i in range(0, num_MEngs_needed):
-			# Pick a random MBA from the list of students who ranked.
-			need_new_student = True
-			while (need_new_student):
-				r = util.random_index(len(project_ranks))
-				random_triple = project_ranks[r]
-				student = random_triple[0]
-				student_type = random_triple[2]
-				if (student_type == "MEng" or student_type == 1):
-					IDs_already_on_project = [s.ID for s in project.students]
-					# Check that this student is not already on this project
-					if (student.ID in IDs_already_on_project):
-						# If they are, then pick another random student.
-						pass
-					else:
-						# They're not on the project, so don't need to look for another student.
-						need_new_student = False
-						# Add the student to the project.
-						project.add_student(student) 
-				else:
-					pass
-
-		print "Project needed " + str(num_MEngs_needed) + " MEngs. And now is "
-		print str(project.ID) + ": " + str([s.ID for s in project.students])	
-
-	# Printing things
-	if (verbose):
-		print_students(feasible_projects)
-
-
-	print "At the end the unmatched students are "
-	print [s.ID for s in unmatched_students]
-
-	# Maybe try this approach
-	# For the unmatched students:
-		# For project in their list of rankings:
-			# If done is false
-				# List all students who ranked this project.
-				# Form a team with these students.
-				# If 
-
-	return (feasible_projects, unmatched_students)
-
 # For the given student, find the project that it was on.
 def find_students_project(student, projects, newly_added_ID):
 	#project_IDs = [p.ID for p in projects]
@@ -578,25 +343,6 @@ def remove_students_from_projects(students_to_remove, projects, ID, remove_from_
 					else:
 						pass
 
-# Find the waitings lists that student is on.
-def find_waiting_lists(student, projects, verbose = False):
-	waiting_lists = []
-	for p in projects:
-		lst = p.waiting_students
-		matching = [tup for tup in lst if tup[1].ID == student.ID]
-		if (verbose):
-			print "For project " + str(p.ID) + ":"
-			print matching
-		# If this project is currently full
-		if ((len(matching) > 0) and (len(p.students) == 4)):
-			waiting_lists.append(p)
-
-	# Sort the project list by the rank that this student gave the project
-	new_lst = sorted(waiting_lists, key = lambda p: student.get_ranking(p.ID))
-	if (verbose):
-		print [p.ID for p in new_lst]
-	return new_lst
-
 if __name__ == "__main__":
 
 	configParser = ConfigParser.ConfigParser()
@@ -607,7 +353,7 @@ if __name__ == "__main__":
 
 	students = util.create_students_from_input("tests.csv")
 	all_projects = util.generate_all_projects()
-	initial_solution(students, all_projects)
+	greedy_initial_solution(students, all_projects)
 	#feasible_projects = util.create_feasible_projects(students)
 
 
